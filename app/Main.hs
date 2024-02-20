5fdeaf83f167e8bea49f7f5e502ce69fc483bc21
Quinten Cabo <tintin10q@hotmail.com>
2024-02-20 18:06:04 +0000

    Now main also prints the formatted program

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import System.Environment (getArgs)
 import Text.Megaparsec (parse, errorBundlePretty)
 import Parser.Program (pProgram)
 import qualified Data.Text.IO as TIO
+import Printer
 
 main :: IO ()
 main = do
@@ -11,4 +12,6 @@ main = do
     source <- TIO.readFile file
     case parse pProgram file source of
         Left err -> putStr (errorBundlePretty err)
-        Right ast -> print ast
+        Right ast -> do 
+                        putStr $ formatProgram ast 
+                        print ast 
